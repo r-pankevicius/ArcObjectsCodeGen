981cf20c1ff0dc22dbe34da299600260d3d9bb28
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2021-11-15 12:10:15 +0200

    Added ArcGisLicenseFixture

---
+++ src/ArcObjectsCodeGen.Runtime/ArcGisLicenseFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArcObjectsCodeGen.Runtime
+{
+	/// <summary>
+	/// To support xUnit IClassFixture<ArcGisLicenseFixture>:
+	/// <code>
+	/// public class MyTestClass : IClassFixture<ArcGisLicenseFixture>
+	/// {
+	/// ...
+	/// }
+	/// </code>
+	/// </summary>
+	public class ArcGisLicenseFixture : IDisposable
+	{
+		readonly ArcGisLicense m_License;
+
+		public ArcGisLicenseFixture()
+		{
+			m_License = ArcGisLicense.GetLicense();
+		}
+
+		public void Dispose() => m_License.Dispose();
+	}
+}
